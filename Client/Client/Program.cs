bc4a0828e8cc4955fd8ebfb067f03a1524f1dbf0
Мария <maaf23@yandex.ru>
2017-12-16 22:35:27 +0500

    !

--- Client/Client/Program.cs
+++ Client/Client/Program.cs
@@ -2,14 +2,57 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
-namespace Client
+using ZeroMQ;
+
+namespace Examples
 {
-    class Program
+    static partial class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            //
+            // Hello World client
+            //
+            // Author: metadings
+            //
+
+            if (args == null || args.Length < 1)
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                args = new string[] { "tcp://127.0.0.1:5555" };
+            }
+
+            string endpoint = args[0];
+
+            // Create
+            using (var context = new ZContext())
+            using (var requester = new ZSocket(context, ZSocketType.REQ))
+            {
+                // Connect
+                requester.Connect(endpoint);
+
+                for (int n = 0; n < 10; ++n)
+                {
+                    string requestText;
+                    Console.Write("Message: ");
+                    requestText = Console.ReadLine();
+                    Console.WriteLine();
+                    Console.Write("Sending {0}…", requestText);
+
+                    // Send
+                    requester.Send(new ZFrame(requestText));
+
+                    // Receive
+                    using (ZFrame reply = requester.ReceiveFrame())
+                    {
+                        Console.WriteLine(" Received: {0} {1}!", requestText, reply.ReadString());
+                    }
+                }
+            }
         }
     }
 }
--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -2,55 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
+using System.Threading.Tasks;
 
-using ZeroMQ;
-
-namespace Examples
+namespace ConsoleApp1
 {
-    static partial class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            //
-            // Hello World server
-            //
-            // Author: metadings
-            //
-
-            if (args == null || args.Length < 1)
-            {
-                Console.WriteLine();
-                Console.WriteLine();
-                Console.WriteLine("Message");
-                Console.WriteLine();
-                args = new string[] { "" };
-            }
-
-            string name = args[0];
-
-            // Create
-            using (var context = new ZContext())
-            using (var responder = new ZSocket(context, ZSocketType.REP))
-            {
-                // Bind
-                responder.Bind("tcp://*:5555");
-
-                while (true)
-                {
-                    // Receive
-                    using (ZFrame request = responder.ReceiveFrame())
-                    {
-                        Console.WriteLine("Received {0}", request.ReadString());
-
-                        // Do some work
-                        Thread.Sleep(1);
-
-                        // Send
-                        responder.Send(new ZFrame(name));
-                    }
-                }
-            }
         }
     }
 }
